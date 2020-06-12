9c8007930fa6f7dcf7d0528e16cb9312289e52e3
hardik455 <kapspatel355@gmail.com>
2020-06-12 11:36:56 +0530

    Configration Change

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,17 @@
+require 'elasticsearch/model'
+
 class Post < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
   
   index_name([Rails.env,base_class.to_s.pluralize.underscore].join('_'))
 
-
-  settings do
-    mappings dynamic: false do
-      indexes :author, type: :text
-      indexes :title, type: :text, analyzer: :english
-      indexes :body, type: :text, analyzer: :english
-      indexes :tags, type: :text, analyzer: :english
-      indexes :published, type: :boolean
-    end
-  end
+    # mappings dynamic: false do
+    #   indexes :author, type: :text
+    #   indexes :title, type: :text, analyzer: :english
+    #   indexes :body, type: :text, analyzer: :english
+    #   indexes :tags, type: :text, analyzer: :english
+    # end
 
   def self.search_published(query)
     self.search({
